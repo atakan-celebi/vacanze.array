a71d6852c5a42ca02222d0d8136597dd35fc4403
Atakan Celebi <atakancelebi2007@gmail.com>
2025-01-04 21:40:08 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    int n[99], user, min = 0, max = 9, ran, con=0;
+    srand(time(NULL));
+    printf("insere il numero");
+    scanf("%d", &user);
+    for (int i = 0; i < 100; i++) {
+        ran = n[i]= rand() % (min - max + 1) + min;
+        if (ran == user) {
+            con++;
+        }
+    }
+    printf("il numero %d e' stato contato %d volte",user,con);
+}
